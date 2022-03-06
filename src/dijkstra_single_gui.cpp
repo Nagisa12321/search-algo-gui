d0f70aa18af2fc513252dcb3378f1f5f5907af9f
jt <cjt807916@gmail.com>
2022-03-06 12:35:19 +0800

    feat: use pointer instead of smart pointer

--- src/dijkstra_single_gui.cpp
+++ src/dijkstra_single_gui.cpp
@@ -6,6 +6,7 @@
 #include <SDL.h>
 #include <random>
 #include <algorithm>
+#include <ostream>
 
 const int block_size = 10;
 
@@ -14,6 +15,7 @@ class Bag;
 class Edge;
 
 class Point {
+    friend std::ostream &operator<<(std::ostream &, const Point &);
 public:
     Point(int x, int y);
     void setParent(Bag *b);
@@ -26,37 +28,38 @@ private:
 
 class Bag {
 public:
-    Bag(std::shared_ptr<Point> p);
-    void merge(std::shared_ptr<Bag> other);
+    Bag(Point *p);
+    void merge(Bag *other);
 private:
-    std::vector<std::shared_ptr<Point>> points;
+    std::vector<Point *> points;
 };
 
 class Edge {
 public:
-    Edge(std::shared_ptr<Point> p1, std::shared_ptr<Point> p2);
-    std::pair<std::shared_ptr<Point>, std::shared_ptr<Point>> getPoints();
+    Edge(Point *p1, Point *p2);
+    std::pair<Point *, Point *> getPoints();
 private:
-    std::shared_ptr<Point> p1;
-    std::shared_ptr<Point> p2;
+    Point *p1;
+    Point *p2;
 };
 
-Edge::Edge(std::shared_ptr<Point> p1, std::shared_ptr<Point> p2)
+Edge::Edge(Point *p1, Point *p2)
     : p1(p1)
     , p2(p2)
 {}
 
-std::pair<std::shared_ptr<Point>, std::shared_ptr<Point>> Edge::getPoints() {
+std::pair<Point *, Point *> Edge::getPoints() {
     return { p1, p2 };
 }
-Bag::Bag(std::shared_ptr<Point> p)
+Bag::Bag(Point *p)
     : points{ p, }
 {
     p->setParent(this);
 }
 
-void Bag::merge(std::shared_ptr<Bag> other) {
-    for (std::shared_ptr<Point> p : other->points) {
+void Bag::merge(Bag *other) {
+    for (Point *p : other->points) {
+        p->setParent(this);
         points.push_back(p);
     }
 }
@@ -75,33 +78,38 @@ Bag *Point::parent() {
     return bag;
 }
 
+
+std::ostream &operator<<(std::ostream &os, const Point &p) {
+    return os << "[" << p.x << ", " << p.y << "]";
+}
+
 int main() {
     int edges;
     std::cout << "How many edges you want to generate? " << std::endl;
     std::cin >> edges;
 
     // Kruskal's Algorithm
-    std::set<std::shared_ptr<Bag>> bags;
-    std::vector<std::shared_ptr<Point>> points;
-    std::vector<std::vector<std::shared_ptr<Point>>> 
-        points_map(edges, std::vector<std::shared_ptr<Point>>(edges));
-    std::vector<std::shared_ptr<Edge>> edges_vec;
+    std::set<Bag *> bags;
+    std::vector<Point *> points;
+    std::vector<std::vector<Point *>> 
+        points_map(edges, std::vector<Point *>(edges));
+    std::vector<Edge *> edges_vec;
 
     for (int x = 0; x < edges; ++x) {
         for (int y = 0; y < edges; ++y) {
-            std::shared_ptr<Point> point = std::make_shared<Point>(x, y);
-            std::shared_ptr<Bag> bag = std::make_shared<Bag>(point);
+            Point *point = new Point(x, y);
+            Bag *bag = new Bag(point);
             bags.insert(bag);
             points.push_back(point);
             points_map[x][y] = point;
             if (x != 0) {
-                std::shared_ptr<Edge> edge = 
-                    std::make_shared<Edge>(point, points_map[x - 1][y]);
+                Edge *edge = 
+                    new Edge(point, points_map[x - 1][y]);
                 edges_vec.push_back(edge);
             }
             if (y != 0) {
-                std::shared_ptr<Edge> edge = 
-                    std::make_shared<Edge>(point, points_map[x][y - 1]);
+                Edge *edge = 
+                    new Edge(point, points_map[x][y - 1]);
                 edges_vec.push_back(edge);
             }
         }
@@ -113,14 +121,39 @@ int main() {
     std::shuffle(edges_vec.begin(), edges_vec.end(), std::default_random_engine());
     auto it = edges_vec.begin();
     while (bags.size() != 1) {
-        std::pair<std::shared_ptr<Point>, std::shared_ptr<Point>> p 
+        std::pair<Point *, Point *> p 
                 = (*it)->getPoints();
         if (p.first->parent() == p.second->parent()) {
             ;
         } else {
-            std::shared_ptr<Bag> other(p.second->parent());
-            p.first->parent()->merge(other);
-            bags.erase(other);
+            Bag *lhs = p.first->parent();
+            Bag *rhs = p.second->parent();
+
+            // add rhs to lhs
+            lhs->merge(rhs);
+
+            // remove rhs
+            bags.erase(rhs);
+            delete rhs;
         }
+        ++it;
+    }
+
+    for (Edge *edge : edges_vec) {
+        auto points = edge->getPoints();
+        std::cout << *points.first << ", " << *points.second << std::endl;
+    }
+
+    //
+    // delete 
+    //
+    for (Point *point : points) {
+        delete point;
+    }
+    for (Bag *bag : bags) {
+        delete bag;
+    }
+    for (Edge *edge : edges_vec) {
+        delete edge;
     }
 }
