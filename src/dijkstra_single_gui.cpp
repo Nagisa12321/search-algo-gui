b1ed95fbd266368949cac0f92b0945add8927776
timjtchen <timjtchen@tencent.com>
2022-03-13 17:37:30 +0800

    feat: insert dijkstra

--- src/dijkstra_single_gui.cpp
+++ src/dijkstra_single_gui.cpp
@@ -1,14 +1,23 @@
+#include "SDL2/SDL_events.h"
+#include "SDL2/SDL_rect.h"
+#include "SDL2/SDL_render.h"
+#include <cmath>
 #include <string>
 #include <iostream>
 #include <vector>
 #include <memory>
 #include <set>
-#include <SDL.h>
+#include <SDL2/SDL.h>
 #include <random>
 #include <algorithm>
 #include <ostream>
+#include <unordered_map>
 
-const int block_size = 20;
+// #define PRINT_MARTIX
+
+const int block_size = 10;
+const int red_dot_size = 8;
+const int i4_huge = 0xffff;
 
 class Point;
 class Bag;
@@ -17,15 +26,19 @@ class Edge;
 class Point {
     friend std::ostream &operator<<(std::ostream &, const Point &);
 public:
-    Point(int x, int y);
+    Point(int x, int y, int id);
     void setParent(Bag *b);
-    Bag *parent();
-    void xy(int *x, int *y);
-    void draw(SDL_Renderer *renderer);
+    Bag *parent() const;
+    void xy(int *x, int *y) const;
+    int getId() const;
+    void draw(SDL_Renderer *renderer) const;
+    void drawRedDot(SDL_Renderer *renderer) const;
+    void center(int *x, int *y) const;
 private:
     int x;
     int y;
     Bag *bag;
+    int id;
 };
 
 class Bag {
@@ -69,17 +82,18 @@ void Bag::merge(Bag *other) {
     }
 }
 
-Point::Point(int x, int y) 
+Point::Point(int x, int y, int id) 
     : x(x)
     , y(y)
     , bag()
+    , id(id)
 {}
     
 void Point::setParent(Bag *b) {
     bag = b;
 }
 
-Bag *Point::parent() {
+Bag *Point::parent() const {
     return bag;
 }
 
@@ -89,7 +103,7 @@ std::ostream &operator<<(std::ostream &os, const Point &p) {
 }
 
 
-void Point::xy(int *x, int *y) {
+void Point::xy(int *x, int *y) const {
     *x = this->x;
     *y = this->y;
 }
@@ -98,14 +112,30 @@ bool Bag::more_than_one_children() {
     return this->points.size() > 1;
 }
 
+int Point::getId() const {
+    return id;
+}
 
-void Point::draw(SDL_Renderer *renderer) {
+void Point::center(int *x, int *y) const {
+    *x = this->x * block_size + block_size / 2;
+    *y = this->y * block_size + block_size / 2;
+}
+
+void Point::draw(SDL_Renderer *renderer) const {
     if (this->bag->more_than_one_children()) {
         SDL_SetRenderDrawColor(renderer, 0xff, 0xff, 0xff, 0xff);
     } else {
         SDL_SetRenderDrawColor(renderer, 0xaa, 0xaa, 0xaa, 0xff);
     }
     SDL_Rect rect{x * block_size, y * block_size, block_size, block_size};
+    SDL_RenderFillRect(renderer, &rect);
+}
+
+void Point::drawRedDot(SDL_Renderer *renderer) const {
+    SDL_SetRenderDrawColor(renderer, 0xff, 0x0, 0x0, 0xff);
+    int _x = x * block_size + (block_size - red_dot_size) / 2;
+    int _y = y * block_size + (block_size - red_dot_size) / 2;
+    SDL_Rect rect{_x, _y, red_dot_size, red_dot_size};
     SDL_RenderFillRect(renderer, &rect);
 }
 
@@ -131,7 +161,8 @@ void Edge::draw(SDL_Renderer *renderer) {
     }
 }
 
-void draw_map(SDL_Renderer *renderer, std::set<Edge *> edges, std::vector<Point *> points) {
+void draw_map(SDL_Renderer *renderer, std::set<Edge *> edges, std::vector<Point *> points, 
+              Point *lhs, Point *rhs, std::vector<Point *> *ways) {
     SDL_SetRenderDrawColor(renderer, 0xff, 0xff, 0xff, 0xff);
     //
     // Set the map white
@@ -143,13 +174,113 @@ void draw_map(SDL_Renderer *renderer, std::set<Edge *> edges, std::vector<Point 
     for (Edge *edge : edges) {
         edge->draw(renderer);
     }
+    if (lhs && rhs) {
+        lhs->drawRedDot(renderer);
+        rhs->drawRedDot(renderer);
+        // draw the ways ... 
+        for (int i = 1; i < ways->size(); ++i) {
+            int x1, x2, y1, y2;
+            (*ways)[i]->center(&x1, &y1);
+            (*ways)[i - 1]->center(&x2, &y2);
+            
+            SDL_SetRenderDrawColor(renderer, 0xff, 0x0, 0x0, 0xff);
+            SDL_RenderDrawLine(renderer, x1, y1, x2, y2);
+        }
+    }
     SDL_RenderPresent(renderer);
+}
+
+void print_matrix(const std::vector<std::unordered_map<int, double>> &ohd, int edges) {
+  int i, j;
+  std::cout << "\n";
+  std::cout << "  Distance matrix:\n";
+  std::cout << "\n";
+  std::cout << "\t";
+  for (i = 0; i < edges; i++)
+    std::cout << "\t[" << i << "]";
+  std::cout << std::endl;
+  for (i = 0; i < edges; i++) {
+    std::cout << "\t[" << i << "]";
+    for (j = 0; j < edges; j++) {
+      if (!ohd[i].count(j)) {
+        std::cout << "\tInf";
+      } else {
+        std::cout << "\t" << ohd[i].at(j);
+      }
+    }
+    std::cout << "\n";
+  }
+}
+static double get_val(const std::vector<std::unordered_map<int, double>> &ohd, int i, int j) {
+  if (!ohd[i].count(j)) return i4_huge;
+  else return ohd[i].at(j);
+}
+
+// return the way between lhs and rhs
+std::vector<int> dijkstra(Point *lhs, Point *rhs, const std::vector<std::unordered_map<int, double>> &ohd) {
+    int nv = ohd.size();
+    std::cout << "dijkstra from Point " << lhs->getId() << " and " << rhs->getId() << std::endl;
+    std::vector<bool> connected(nv, false);
+    std::vector<int> point_to(nv, -1);
+    double *dist_to;
+    int i;
+    int step;
+    int mv; // the index of the nearest unconnected node.
+    double md; // the distance from node 0 to the nearest unconnected node.
+    
+    connected[lhs->getId()] = true;
+    dist_to = new double[nv];
+  
+    // init dist_to to one_step distance;
+    for (i = 0; i < nv; ++i)
+        dist_to[i] = get_val(ohd, lhs->getId(), i);
+  
+    // every step will find a new node connect to the tree.
+    for (step = 1; step < nv; ++step) {
+        md = i4_huge;
+        mv = -1;
+        for (i = 0; i < nv; ++i) {
+            if (!connected[i] && dist_to[i] < md) {
+                md = dist_to[i];
+                mv = i;
+            }
+        }
+  
+        // can not find any vertex
+        if (mv == -1) {
+            std::cout << "\n";
+            std::cout << "DIJKSTRA_DISTANCE - Warning!\n";
+            std::cout << "  Search terminated early.\n";
+            std::cout << "  Graph might not be connected.\n";
+            break;
+        }
+  
+        // find a good vertex, so mark it connected.
+        connected[mv] = true;
+  
+        // update the dist_to (relax the finded vertex)
+        for (i = 0; i < nv; ++i) {
+            // the connected can be the best
+            if (!connected[i]) {
+                if (dist_to[i] > dist_to[mv] + get_val(ohd, mv, i)) {
+                    dist_to[i] = dist_to[mv] + get_val(ohd, mv, i);
+                    point_to[i] = mv; 
+                }
+            }
+        }
+    }
+
+    delete [] dist_to;
+    return point_to;
 }
 
 int main() {
     int edges;
     std::cout << "How many edges you want to generate? " << std::endl;
     std::cin >> edges;
+
+    // The dijkstra map 
+    std::vector<std::unordered_map<int, double>> ohd(edges * edges);
 
     // 
     // Init SDL 
@@ -165,7 +296,6 @@ int main() {
     // 
     // Create window and renderer
     //
-    
     // Create an application window with the following settings:
     window = SDL_CreateWindow(
         "An SDL2 window",                  // window title
@@ -177,7 +307,7 @@ int main() {
     );
 
     renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-
+    Point *lhs = nullptr, *rhs = nullptr;
 
     // Kruskal's Algorithm
     std::set<Bag *> bags;
@@ -186,9 +316,10 @@ int main() {
         points_map(edges, std::vector<Point *>(edges));
     std::vector<Edge *> edges_vec;
 
-    for (int x = 0; x < edges; ++x) {
-        for (int y = 0; y < edges; ++y) {
-            Point *point = new Point(x, y);
+    int current_id = 0;
+    for (int y = 0; y < edges; ++y) {
+        for (int x = 0; x < edges; ++x) {
+            Point *point = new Point(x, y, current_id++);
             Bag *bag = new Bag(point);
             bags.insert(bag);
             points.push_back(point);
@@ -210,21 +341,43 @@ int main() {
     // 
     // draw the map 
     //
-    draw_map(renderer, edges_to_draw, points);
+    draw_map(renderer, edges_to_draw, points, lhs, rhs, nullptr);
     
     // 
     // shuffer the edge array
     //
     std::shuffle(edges_vec.begin(), edges_vec.end(), std::default_random_engine());
     auto it = edges_vec.begin();
-    while (bags.size() != 1) {
+
+    SDL_Event ev;
+    bool running = true;
+
+    // 
+    // in the event loop, 
+    // should init the dijkstra map  
+    // 
+    while (bags.size() != 1 && running) {
+        // handle event (QUIT)
+        while (SDL_PollEvent(&ev) != 0) {
+            if (ev.type == SDL_QUIT) {
+                running = false;
+            }
+        }
+
         std::pair<Point *, Point *> p 
                 = (*it)->getPoints();
         if (p.first->parent() == p.second->parent()) {
+            // they are in the same bag!!!
+            // do nothing 
             ;
         } else {
             Bag *lhs = p.first->parent();
             Bag *rhs = p.second->parent();
+
+            // connect this two point. 
+            // the distance is 1.0
+            ohd[p.first->getId()][p.second->getId()] = 1.0;
+            ohd[p.second->getId()][p.first->getId()] = 1.0;
 
             // add rhs to lhs
             lhs->merge(rhs);
@@ -237,14 +390,77 @@ int main() {
         }
         ++it;
 
-        SDL_Delay(8);
+        // SDL_Delay(1);
 
-        draw_map(renderer, edges_to_draw, points);
+        draw_map(renderer, edges_to_draw, points, lhs, rhs, nullptr);
     }
 
-    for (Edge *edge : edges_vec) {
-        auto points = edge->getPoints();
-        std::cout << *points.first << ", " << *points.second << std::endl;
+    // print the matrix
+#ifdef PRINT_MARTIX
+    print_matrix(ohd, edges * edges);
+#endif
+
+    // for (Edge *edge : edges_vec) {
+    //     auto points = edge->getPoints();
+    //     std::cout << *points.first << ", " << *points.second << std::endl;
+    // }
+
+    // 
+    // click sensor!!
+    // 
+    running = true;
+
+    std::vector<Point *> points_;
+    while (running) {
+        draw_map(renderer, edges_to_draw, points, lhs, rhs, &points_);
+        // handle event (QUIT)
+        while (SDL_PollEvent(&ev) != 0) {
+            if (ev.type == SDL_QUIT) {
+                running = false;
+            } else if (ev.type == SDL_MOUSEBUTTONDOWN) {
+                int mouseX = ev.motion.x;
+                int mouseY = ev.motion.y;
+                
+                int modX = mouseX / block_size;
+                int modY = mouseY / block_size;
+
+                Point *p = points_map[modX][modY];
+                std::cout << "mouse down! mouse.x = " 
+                          << mouseX << ", mouse.y = " << mouseY 
+                          << ", [" << modX << ", " << modY << "]" 
+                          << ", point.id = " << p->getId() << std::endl;
+                
+
+                if (!lhs) {
+                    lhs = p;
+                } else if (!rhs) {
+                    rhs = p;
+
+                    std::vector<int> point_to = dijkstra(lhs, rhs, ohd);
+                    points_.clear();
+                    points_.push_back(rhs);
+
+                    std::cout << std::endl;
+                    int i = rhs->getId();
+                    for (;;) {
+                        int id = point_to[i];
+                        std::cout << "point to " << i << " is " << id << std::endl;
+                        if (id == -1 || id == lhs->getId()) {
+                            break;
+                        } else {
+                            i = id;
+                        }
+                        points_.push_back(points[i]);
+                    }
+                    points_.push_back(lhs);
+                } else {
+                    lhs = p;
+                    rhs = nullptr;
+                }
+            }
+        }
+
+        SDL_Delay(100);
     }
 
     //
@@ -259,8 +475,6 @@ int main() {
     for (Edge *edge : edges_vec) {
         delete edge;
     }
-    // delay 3 s
-    SDL_Delay(3000);
 
     // 
     // Destory window and renderer
